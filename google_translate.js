fbf5fa843ee50d1cf128b0fe37d7b9ee09cc698b
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2014-11-16 23:03:26 +0100

    Google translate.

--- google_translate.js
+++ google_translate.js
@@ -1,3 +1,32 @@
-function googleTranslate(text, fromCode, toCode, callback) {
-  callback(toCode, text);
+function googleTranslate(text, fromCode, toCode) {
+  if (fromCode === toCode) {
+    return new $.Deferred().resolve( {translation:text, transliteration:''} )
+  }
+
+  //https://web.archive.org/web/20140408043100/http://www.yqlblog.net/blog/2010/03/12/avoiding-rate-limits-and-getting-banned-in-yql-and-pipes-caching-is-your-friend/
+  var yqlEndpoint = 'https://query.yahooapis.com/v1/public/yql?'
+  var query = yqlEndpoint + 
+    'q=select * from google.translate where ' +
+    'q="' + text + '" ' +
+    'and target="' + toCode + '" ' +
+    'and source="' + fromCode + '";' +
+    '&format=json' +
+    '&env=store://datatables.org/alltableswithkeys' +
+    '&_maxage=' + 60*60*24 //cache for a day
+
+  function filter(data, dataType) {
+    var obj = $.parseJSON(data)
+    try {
+      var translation = obj.query.results.json.sentences;
+      return {translation:translation.trans, transliteration:translation.translit};
+    } catch (e) {
+      return {translation:undefined, transliteration:undefined};
+    }
+  }
+
+  return $.ajax({
+    url: query,
+    dataFilter: filter,
+    dataType: 'text', //we already parse to JSON in the datafilter
+  });
 }
