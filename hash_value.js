7d52e5206001991b5b0b45e3d7f40b5afff3278e
Christoph Matthies <christoph.matthies@gmail.com>
2014-11-18 23:47:27 +0100

    Pretty up examples box.

--- hash_value.js
+++ hash_value.js
@@ -1,13 +1,13 @@
-var getHashValue = function(key) {
+  var getHashValue = function(key) {
     if (typeof key !== 'string') return null;
     key = key.toLowerCase();
     var hash = location.hash.toLowerCase();
     var match = hash.match(new RegExp(key + '=([^&]*)'));
     if (match) return match[1].replace(/\+/g, ' ');
     return null;
-};
+  };
 
-var updateHashValue = function(key, value) {
+  var updateHashValue = function(key, value) {
     var currentValue = getHashValue(key);
     var obj = {};
     obj[key] = value;
@@ -15,13 +15,16 @@ var updateHashValue = function(key, value) {
     var currentHash = location.hash;
     //the key is not present
     if ( currentValue === null ) {
-        var newHash = '';
-        //if no '#'' is present, add it
-        if ( currentHash.indexOf('#') === -1) newHash = '#';
-        location.hash += (newHash + param);
-        return
+      var newHash = '';
+      //if no '#'' is present, add it
+      if ( currentHash.indexOf('#') === -1) newHash = '#';
+      location.hash += (newHash + param);
+      return
     }
     //the key is already present
     obj[key] = currentValue;
-    location.hash = currentHash.replace($.param(obj), param);
-}
+    location.hash = currentHash.replace(
+      $.param(obj).toLowerCase(),
+      param.toLowerCase()
+    );
+  }
